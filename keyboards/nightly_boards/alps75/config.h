be9cc32f20c4928551108eee9162a904b3b26c16
Neil Brian Ramirez <nightlyboards@gmail.com>
2022-09-02 21:40:17 +0800

    add alps75 folder

---
+++ keyboards/nightly_boards/alps75/config.h
@@ -0,0 +1,46 @@
+/*
+Copyright 2022 DeskDaily
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+#define MATRIX_ROW_PINS { B0, B1, B2, B3, B7, F7 }
+#define MATRIX_COL_PINS { F0, F1, F4, F5, F6, D0, D1, D2, D3, D5, D4, D6, D7, B4, B5 }
+
+#define DIODE_DIRECTION COL2ROW
+
+/* Encoders */
+
+#define ENCODERS_PAD_A { B6 }
+#define ENCODERS_PAD_B { C6 }
+#define ENCODER_RESOLUTION 6
+#define TAP_CODE_DELAY 10
+#define ENCODER_DIRECTION_FLIP
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* define if matrix has ghost (lacks anti-ghosting diodes) */
+//#define MATRIX_HAS_GHOST
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+
