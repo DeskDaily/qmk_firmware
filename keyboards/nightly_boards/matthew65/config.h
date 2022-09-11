bec2b9cd0b8746b835342e20a63586e63cc7a957
Neil Brian Ramirez <nightlyboards@gmail.com>
2022-09-11 08:40:16 +0800

    add matthew65

---
+++ keyboards/nightly_boards/matthew65/config.h
@@ -0,0 +1,65 @@
+/*
+Copyright 2022 DeskDaily
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 17
+
+/* Encoders */
+
+#define ENCODERS_PAD_A { A2, A0, C0 }
+#define ENCODERS_PAD_B { A1, C3, C1 }
+#define ENCODER_RESOLUTION 4
+#define TAP_CODE_DELAY 10
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+ */
+#define MATRIX_ROW_PINS { C6, B15, B14, B12, B5 }
+#define MATRIX_COL_PINS { C2, A4, B0, C5, C4, A7, A6, A5, B13, C9, C8, C7, C11, C12, B4, B3, D2 }
+
+#define RGB_DI_PIN A3
+#ifdef RGB_DI_PIN
+#define RGBLED_NUM 65
+#define RGBLIGHT_ANIMATIONS
+#endif
+
+/* COL2ROW, ROW2COL*/
+#define DIODE_DIRECTION COL2ROW
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* define if matrix has ghost (lacks anti-ghosting diodes) */
+//#define MATRIX_HAS_GHOST
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+
