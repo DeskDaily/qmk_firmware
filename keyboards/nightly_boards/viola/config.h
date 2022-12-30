1a2db24de2223168a9f9e3f911f55bf4887a186e
Neil Brian Ramirez <nightlyboards@gmail.com>
2022-12-30 18:40:37 +0800

    edited config.h

--- keyboards/nightly_boards/viola/config.h
+++ keyboards/nightly_boards/viola/config.h
@@ -40,6 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define OLED_IC OLED_IC_SH1106
 #define OLED_DISPLAY_128X64
 #define OLED_COLUMN_OFFSET 2
+#define OLED_TIMEOUT 300000
 
 
 /* Encoders */
